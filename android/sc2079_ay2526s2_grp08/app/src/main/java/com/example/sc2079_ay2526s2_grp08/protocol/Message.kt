b015c0252bdf99a263ee38dd99010df65fc217ec
HXong <onghongxun@gmail.com>
2026-01-27 01:47:10 +0800

    Added Protocol parsing support for AMD Tool scripts. Possible extension to RPI for future development on the robot

---
+++ android/sc2079_ay2526s2_grp08/app/src/main/java/com/example/sc2079_ay2526s2_grp08/protocol/Message.kt
@@ -0,0 +1,20 @@
+package com.example.sc2079_ay2526s2_grp08.protocol
+
+//Incoming messages
+sealed interface Incoming {
+    data class RobotPosition(val x: Int, val y: Int, val directionDeg: Int) : Incoming
+    data class GridHex(val hex: String) : Incoming
+    data class GridBinary(val width: Int, val height: Int, val cells: BooleanArray) : Incoming
+
+    data class AddObstacle(val x: Int, val y: Int) : Incoming
+    data class RemoveObstacle(val x: Int, val y: Int) : Incoming
+
+    data class Msg(val text: String) : Incoming
+    data class Raw(val line: String) : Incoming
+}
+
+//Outgoing messages
+sealed interface Outgoing {
+    data class Raw(val line: String) : Outgoing
+    data class Move(val code: String) : Outgoing
+}
